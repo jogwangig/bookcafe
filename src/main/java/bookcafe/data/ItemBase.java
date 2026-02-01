d8406ebeeac23f1b5db8f01f93bddfa67c079627
조광익 <@DESKTOP-V6DF01S>
2026-02-01 18:55:31 +0900

    add mapped-super-class ItemBase for user-generated items

---
+++ src/main/java/bookcafe/data/ItemBase.java
@@ -0,0 +1,23 @@
+package bookcafe.data;
+
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import bookcafe.data.entity.SiteUser;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@MappedSuperclass
+@ToString(callSuper = true)
+@EntityListeners(AuditingEntityListener.class)
+public class ItemBase extends Base{
+	
+	@CreatedBy
+	@ManyToOne
+	private SiteUser siteUser;
+
+}
