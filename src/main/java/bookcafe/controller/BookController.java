884e139657b3658fc54197073d4e6f8db5918226
조광익 <hobit9801@gmail.com>
2026-02-10 16:08:25 +0900

    insert book info into form with bookshelf info to register

--- src/main/java/bookcafe/controller/BookController.java
+++ src/main/java/bookcafe/controller/BookController.java
@@ -1,18 +1,21 @@
 package bookcafe.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import bookcafe.data.entity.Book;
+import bookcafe.data.entity.BookShelf;
 import bookcafe.data.repository.BookRepository;
 import bookcafe.data.repository.BookShelfRepository;
 import bookcafe.data.valueobject.BookInfo;
+import bookcafe.security.CustomUserDetails;
 import lombok.AllArgsConstructor;
 
 @Controller
@@ -25,16 +28,21 @@ public class BookController {
 	BookShelfRepository bookShelfRepo;
 	
 	@GetMapping("/create")
-	public String createBook(Model model) {
+	public String createBook(Model model, @AuthenticationPrincipal CustomUserDetails userDetails) {
+		
+		List<BookShelf> bookShelfs = bookShelfRepo.findByUserId(userDetails.getId());
+
 		model.addAttribute("bookInfo", new BookInfo());
+		model.addAttribute("bookShelfs", bookShelfs);
 		return "/form/book-creation-form";
 	}
 	
 	@PostMapping("/create")
-	public String createBook(@ModelAttribute("bookInfo")BookInfo bookInfo) {
+	public String createBook(@ModelAttribute("bookInfo")BookInfo bookInfo, @ModelAttribute("bookshelf-select")Long bookShelfId) {
+		
 		bookRepo.save(Book.builder().
 				bookInfo(bookInfo).
-				bookShelf(bookShelfRepo.findById(1L).get()).build());
+				bookShelf(bookShelfRepo.getReferenceById(bookShelfId)).build());
 		return "redirect:/";
 	}
 }
--- src/main/java/bookcafe/controller/ControllerExceptionHandler.java
+++ src/main/java/bookcafe/controller/ControllerExceptionHandler.java
@@ -1,6 +1,5 @@
 package bookcafe.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
