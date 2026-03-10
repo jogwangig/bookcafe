86bbd95fe04f111d0f507260a4c6447be0006b01
조광익 <hobit9801@gmail.com>
2026-03-10 11:18:01 +0900

    add EmailAuthentication Entity for user email authentication

---
+++ src/main/java/bookcafe/data/entity/EmailAuthentication.java
@@ -0,0 +1,18 @@
+package bookcafe.data.entity;
+
+import bookcafe.data.Base;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+public class EmailAuthentication extends Base{
+	
+	private String emailAddress;
+	
+	private boolean isAuthenticated;
+}
